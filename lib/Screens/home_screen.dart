e1a372921e874e6528a83b09bd34a93e4244fde0
MostafaAmin0 <mostafa.aminmk@gmail.com>
2024-03-18 01:08:17 +0200

    start of lab 6

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,49 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:lab_g2/Screens/contact_list_screen.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _count = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _count = 0;
-    print('initState is called');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print('build is called');
     return Scaffold(
       appBar: AppBar(
         title: const Text("Profile"),
         centerTitle: true,
         backgroundColor: Colors.lightBlue,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _count++;
-          });
-          print(_count);
-        },
-        child: const Icon(Icons.plus_one),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Text("$_count"),
-        ),
+      body: const SafeArea(
+        child: ContactListScreen(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lab_g2/Screens/contact_list_screen.dart';
 import 'package:lab_g2/Screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ContactListScreen(),
+      home: HomeScreen(),
     );
   }
 }
