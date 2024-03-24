029c50688b37f324e731bd3cebf9eeee3bf3cd29
MostafaAmin0 <mostafa.aminmk@gmail.com>
2024-03-25 01:06:17 +0200

    add text fields

--- lib/Screens/add_contact_screen.dart
+++ lib/Screens/add_contact_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class AddContactScreen extends StatelessWidget {
+import '../Model/MyContacts.dart';
+import '../Model/contact.dart';
+
+class AddContactScreen extends StatefulWidget {
   const AddContactScreen({super.key});
+
+  @override
+  State<AddContactScreen> createState() => _AddContactScreenState();
+}
+
+class _AddContactScreenState extends State<AddContactScreen> {
+  late TextEditingController nameCtrlr;
+  late TextEditingController phoneCtrlr;
+  late TextEditingController mailCtrlr;
+
+  @override
+  void dispose() {
+    nameCtrlr.dispose();
+    mailCtrlr.dispose();
+    phoneCtrlr.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    nameCtrlr = TextEditingController();
+    phoneCtrlr = TextEditingController();
+    mailCtrlr = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +39,44 @@ class AddContactScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.lightBlue,
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Add Contact"),
-          onPressed: () {},
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              fillColor: Colors.lightBlue,
+              hintText: 'Name',
+            ),
+            controller: nameCtrlr,
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              fillColor: Colors.lightBlue,
+              hintText: 'mail',
+            ),
+            controller: mailCtrlr,
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              fillColor: Colors.lightBlue,
+              hintText: 'phone',
+            ),
+            controller: phoneCtrlr,
+          ),
+          Center(
+            child: ElevatedButton(
+              child: const Text("Add Contact"),
+              onPressed: () {
+                MyContacts.contacts.add(Contact(
+                  nameCtrlr.value.text,
+                  mailCtrlr.value.text,
+                  phoneCtrlr.value.text,
+                ));
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
